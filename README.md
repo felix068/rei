b82bcfacc9bcecf6b5d77d819f0e6323414b78ec
GMkonan <guilhemont@hotmail.com>
2025-06-27 21:58:29 -0300

    Add test icons

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Rei
 
+
+<div align="center">
+<img src="./web/public/logo-icon.png" width="240px" height="240px" />
+</div>
+
+
+<div align="center">
+<span> <strong>in need of a better logo</strong> 😭</span>
+</div>
+
 (R)ss (E)xtraordinary Amaz(I)ng (reader)
 
 That's REI! A fast and simple rss reader for local usage
--- web/src/layouts/main-layout.tsx
+++ web/src/layouts/main-layout.tsx
@@ -4,7 +4,7 @@ export default function MainLayout() {
 	return (
 		<div className="flex flex-col justify-center items-center">
 			<div className="flex gap-x-4">
-				<h1>Rei</h1>
+				<img src="/public/logo-icon.png" width={72} height={72} />
 				<button className="font-medium">
 					Unread <span className="font-bold">(1)</span>
 				</button>
